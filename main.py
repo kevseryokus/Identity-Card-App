f01215f70488296b91472429e22d5b1bd4511394
KEVSER YOKUS <kevseryokuss@gmail.com>
2022-11-17 00:56:48 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+name = "Kevser"
+surname = "Yokuş"
+birth = "18.03.1999"
+place_birth = "Karabük"
+age = "23"
+
+print("-------------")
+print("MY IDENTİTY CARD")
+print("Name         :",name)
+print("Surname      :",surname)
+print("Birth        :",birth)
+print("Place_Birth  :",place_birth)
+print("Age          :",age)
+print("--------------")
